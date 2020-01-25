7848193c30ebb3ae068bd11b5797aad28d9f7c4e
guchengming <guchengming@ewell.cc>
2020-01-25 15:40:36 +0800

    提交

--- jest.config.js
+++ jest.config.js
@@ -2,12 +2,18 @@
 module.exports = {
     verbose: true,
      clearMocks: false,
-     collectCoverage: false,
-     reporters: ['default'],
+     collectCoverage: true,
+     
+     reporters: ['default','jest-junit'],
+     collectCoverageFrom:["lib/**/*.{ts,tsx}","!**/node_modules/**"],
+     coverageDirectory:'coverage',
+     coverageReporters:['text','lcov'],
+
      moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
      moduleDirectories: ['node_modules'],
      moduleNameMapper: {
-        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/test/__mocks__/file-mock.js","\\.(css|less|sass|scss)$":"<rootDir>/test/__mocks__/object-mock.js"
+        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/test/__mocks__/file-mock.js",
+        "\\.(css|less|sass|scss)$":"<rootDir>/test/__mocks__/object-mock.js"
      },
      // 所有的测试文件在哪里
      testMatch: ['<rootDir>/**/__tests__/**/*.unit.(js|jsx|ts|tsx)'],
