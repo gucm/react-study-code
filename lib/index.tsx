79d8f697239e698808e9fdcc3945ad11070d33a8
guchengming <guchengming@ewell.cc>
2020-01-26 19:09:48 +0800

    publish

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,8 +1,1 @@
-import reactDOM from 'react-dom';
-import React from 'react';
-import Icon from './icon/icon';
-const onClick:React.MouseEventHandler = (e) => {
-    console.log(e)
-    console.log((e.target as SVGUseElement).href)
-}
-reactDOM.render(<Icon name='alipay' onClick={onClick}/>,document.querySelector('#root'))
+export {default as Icon} from './icon/icon';
